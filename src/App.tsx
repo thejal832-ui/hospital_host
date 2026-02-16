2380541bc43adb3c62d8e69abbb0e991058082c2
thejal.s <thejalempty123develop@gmail.com>
2026-02-16 18:15:47 +0530

    app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from "./assets/react.svg";
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
       <div>
+        <BrowserRouter>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        {/* <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
+        </BrowserRouter>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="App">
+
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -33,3 +38,4 @@ function App() {
 }
 
 export default App
+
